18955f786185fb9028c4465c9bd6be5b1d41905b
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-05-16 13:34:40 -0400

    submission

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -51,19 +51,14 @@ let spaceShuttle = new Spacecraft("Determination", 17500);
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-console.log(
-  `It will take ${spaceShuttle.name} ${spaceShuttle.getDaysToLocation(
-    kilometersToMars
-  )} days to reach Mars`
-);
-console.log(
-  `It will take ${spaceShuttle.name} ${spaceShuttle.getDaysToLocation(
-    kilometersToTheMoon
-  )} days to reach the Moon.`
-);
+
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
 
 // Add the printDaysToLocation function to the Spacecraft class.
 
 // Paste in the code from step 6 here:
+spaceShuttle.printDaysToLocation(new SpaceLocation("Mars", kilometersToMars));
+spaceShuttle.printDaysToLocation(
+  new SpaceLocation("the Moon", kilometersToTheMoon)
+);
